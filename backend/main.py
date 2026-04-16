89e73bc2bf97c41f8480f7d820a923b8287640ba
Hard_07 <79440724+Hard210207@users.noreply.github.com>
2026-04-16 23:06:21 +0530

    Fixed CORS to allow Vercel

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # This is the magic bridge that allows your React app to talk to Python
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
